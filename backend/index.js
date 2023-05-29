f745e220f27c4e6629a7c1e1f236ebe1f9aa4ec6
Tausif4171 <tausifkhan4171@gmail.com>
2023-05-29 13:21:57 +0530

    updated packages

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors' // middleware in between frontend and backend
+import bodyParser from 'body-parser'
+
+const app = express()
+const port = 5000
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/',(req,res)=>res.send('Hello from express'))
