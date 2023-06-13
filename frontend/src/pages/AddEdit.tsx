5778bc1e718479324074c4dafe30851c1e263ee2
Tausif4171 <tausifkhan4171@gmail.com>
2023-06-13 08:58:04 +0530

    form created for handling details 📛

--- frontend/src/pages/AddEdit.tsx
+++ frontend/src/pages/AddEdit.tsx
@@ -3,14 +3,34 @@ import React, { useState } from 'react'
 const initialState = {
   name: '',
   email: '',
-  phoneNumber: null
+  phoneNumber: ''
 }
 
 function AddEdit() {
   const [state, setState] = useState(initialState)
   const { name, email, phoneNumber } = initialState
+  function handleInputChange() {
+
+  }
+
+  function handleSubmit() {
+
+  }
+
   return (
-    <div>AddEdit</div>
+    <div>
+      <form onSubmit={handleSubmit} className='w-[100%]'>
+        <label>Name</label>
+        <input type='text' name='name' value={name} placeholder='Please enter your name' onChange={handleInputChange} />
+
+        <label>Email</label>
+        <input type='email' name='email' value={email} placeholder='Please enter your email' onChange={handleInputChange} />
+
+        <label>Phone Number</label>
+        <input type='number' name='phoneNumber' value={phoneNumber} placeholder='Please enter your phoneNumber' onChange={handleInputChange} />
+
+      </form>
+    </div>
   )
 }
 
