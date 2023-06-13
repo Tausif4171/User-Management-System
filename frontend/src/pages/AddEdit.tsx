5378e8111ad0c7e69185a04f462838de47406c19
Tausif4171 <tausifkhan4171@gmail.com>
2023-06-13 08:49:42 +0530

    declare initial state

--- frontend/src/pages/AddEdit.tsx
+++ frontend/src/pages/AddEdit.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initialState = {
+  name: '',
+  email: '',
+  phoneNumber: null
+}
 
 function AddEdit() {
+  const [state, setState] = useState(initialState)
+  const { name, email, phoneNumber } = initialState
   return (
     <div>AddEdit</div>
   )
