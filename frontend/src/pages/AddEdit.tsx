6d3e711dab0fb1771810357dd11307f18140b888
Tausif4171 <tausifkhan4171@gmail.com>
2023-06-15 19:51:11 +0530

    update form ❄️

--- frontend/src/pages/AddEdit.tsx
+++ frontend/src/pages/AddEdit.tsx
@@ -18,19 +18,26 @@ function AddEdit() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className='w-[100%]'>
-        <label>Name</label>
-        <input type='text' name='name' value={name} placeholder='Please enter your name' onChange={handleInputChange} />
+    <div className="flex flex-col  items-center mt-[120px] w-full h-screen leading-10">
+      <form onSubmit={handleSubmit} className="flex flex-col w-1/5">
+        <div className='mb-[5px] flex flex-col'>
+          <label>Name</label>
+          <input type='text' name='name' value={name} placeholder='Please enter your name' onChange={handleInputChange} className='border-[1px] border-[#706e6e] border-solid rounded-[4px] text-[12px] px-2' />
+        </div>
 
-        <label>Email</label>
-        <input type='email' name='email' value={email} placeholder='Please enter your email' onChange={handleInputChange} />
+        <div className='mb-[5px] flex flex-col'>
+          <label>Email</label>
+          <input type='email' name='email' value={email} placeholder='Please enter your email' onChange={handleInputChange} className='border-[1px] border-[#706e6e] border-solid rounded-[4px] text-[12px] px-2' />
+        </div>
 
-        <label>Phone Number</label>
-        <input type='number' name='phoneNumber' value={phoneNumber} placeholder='Please enter your phoneNumber' onChange={handleInputChange} />
+        <div className='mb-[5px] flex flex-col'>
+          <label>Phone Number</label>
+          <input type='text' name='phoneNumber' value={phoneNumber} placeholder='Please enter your phoneNumber' onChange={handleInputChange} className='border-[1px] border-[#706e6e] border-solid rounded-[4px] text-[12px] px-2' />
 
+        </div>
       </form>
     </div>
+
   )
 }
 
