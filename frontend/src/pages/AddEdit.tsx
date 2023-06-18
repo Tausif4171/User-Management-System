99a342e2841221e267b9d3c00b37701cd05abe08
Tausif4171 <tausifkhan4171@gmail.com>
2023-06-18 21:41:30 +0530

    handling inputs 📍

--- frontend/src/pages/AddEdit.tsx
+++ frontend/src/pages/AddEdit.tsx
@@ -8,13 +8,17 @@ const initialState = {
 
 function AddEdit() {
   const [state, setState] = useState(initialState)
-  const { name, email, phoneNumber } = initialState
-  function handleInputChange() {
+  console.log(state)
 
+  // const { name, email, phoneNumber } = initialState
+
+  function handleInputChange(e: any) {
+    const { name, value } = e.target
+    setState({ ...state, [name]: value })
   }
 
-  function handleSubmit() {
-
+  function handleSubmit(e: any) {
+    e.preventDefault()
   }
 
   return (
@@ -22,19 +26,20 @@ function AddEdit() {
       <form onSubmit={handleSubmit} className="flex flex-col w-1/5">
         <div className='mb-[5px] flex flex-col'>
           <label>Name</label>
-          <input type='text' name='name' value={name} placeholder='Please enter your name' onChange={handleInputChange} className='border-[1px] border-[#706e6e] border-solid rounded-[4px] text-[12px] px-2' />
+          <input type='text' name='name' value={state.name} placeholder='Please enter your name' onChange={handleInputChange} className='border-[1px] border-[#706e6e] border-solid rounded-[4px] text-[12px] px-2 outline-none' />
         </div>
 
         <div className='mb-[5px] flex flex-col'>
           <label>Email</label>
-          <input type='email' name='email' value={email} placeholder='Please enter your email' onChange={handleInputChange} className='border-[1px] border-[#706e6e] border-solid rounded-[4px] text-[12px] px-2' />
+          <input type='email' name='email' value={state.email} placeholder='Please enter your email' onChange={handleInputChange} className='border-[1px] border-[#706e6e] border-solid rounded-[4px] text-[12px] px-2 outline-none' />
         </div>
 
         <div className='mb-[5px] flex flex-col'>
           <label>Phone Number</label>
-          <input type='text' name='phoneNumber' value={phoneNumber} placeholder='Please enter your phoneNumber' onChange={handleInputChange} className='border-[1px] border-[#706e6e] border-solid rounded-[4px] text-[12px] px-2' />
+          <input type='text' name='phoneNumber' value={state.phoneNumber} placeholder='Please enter your phoneNumber' onChange={handleInputChange} className='border-[1px] border-[#706e6e] border-solid rounded-[4px] text-[12px] px-2 outline-none' />
 
         </div>
+        <button className='border-[1px] border-[#b7b5b5] border-solid w-[100%] mt-7 px-1 rounded text-[16px] bg-[#764293] text-slate-50'>Add</button>
       </form>
     </div>
 
