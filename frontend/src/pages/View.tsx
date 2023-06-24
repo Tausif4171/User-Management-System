9bcfd2a366a056751c70e3c00e79145bdd0e1e42
Tausif4171 <tausifkhan4171@gmail.com>
2023-06-24 21:58:24 +0530

    css added to view component 🎨

--- frontend/src/pages/View.tsx
+++ frontend/src/pages/View.tsx
@@ -18,13 +18,20 @@ function View() {
   }, [id])
   return (
     <div className='flex flex-row justify-center items-center w-[100%] mt-[100px]'>
-      <div className=' flex flex-col border-[1px] border-[#4b4848] border-solid p-6'>
-        <span>id: {id}</span>
-        <span>Name: {state.name && state.name}</span>
-        <span>Email: {state.email && state.email}</span>
-        <span>Phone Number: {state.phoneNumber && state.phoneNumber}</span>
+      <div className=' flex flex-col border-[1px] border-[#4b4848] border-solid'>
+        <div className='flex justify-center items-center mb-4 py-2 text-[#ffffff] text-[20px] bg-[#474545] font-bold'>
+          <h1>User Contact Detail</h1>
+        </div>
+
+        <div className='p-6 flex flex-col text-[16px]'>
+          <span className='font-medium'>Id: <span className='text-[#343232] font-normal'>{id}</span></span>
+          <span className='font-medium'>Name: <span className='text-[#343232] font-normal'>{state.name && state.name}</span></span>
+          <span className='font-medium'>Email: <span className='text-[#343232] font-normal'>{state.email && state.email}</span></span>
+          <span className='font-medium'>Phone Number: <span className='text-[#343232] font-normal'>{state.phoneNumber && state.phoneNumber}</span></span>
+        </div>
+
         <div className='flex justify-center items-center'>
-          <Link to={'/'}><button className='bg-[#444242]'>Go Back</button></Link>
+          <Link to={'/'}><button className='bg-[#000000] px-4 rounded-md h-8 text-[#fff] text-[14px] mb-4'>Go Back</button></Link>
         </div>
       </div>
     </div>
